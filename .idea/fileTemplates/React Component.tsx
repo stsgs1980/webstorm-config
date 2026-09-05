cdf117cea4e9d3e4264253392230d8c2e33301d6
stsgs1980 <stsgs1980@users.noreply.github.com>
2026-09-05 16:23:26 +0300

    feat: add Live Templates, File Templates, and Copyright

---
+++ .idea/fileTemplates/React Component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+export const Component: React.FC<Props> = () => {
+  return (
+    <div>
+      Component
+    </div>
+  );
+};
+
+export default Component;
